0282cbcee4e35561c952013c9c769cb8f3053202
Irfansyed0611 <syedirfan0611@gmail.com>
2025-12-27 10:42:18 +0000

    Test python script

---
+++ Python-Files/test.py
@@ -0,0 +1,14 @@
+# def main():
+#     x = int(input("What's the value of x? "))
+#     print("x squared is", square(x))
+#
+# def square(x):
+#     return x*x
+
+
+def add_numbers(a,b):
+    return a + b
+
+result = add_numbers(1,2)
+print(result)
+
